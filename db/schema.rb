3f191046935bfc982f58f2691a375ce5e42878e8
Bindles <bindlez@gmail.com>
2023-08-21 23:27:48 -0400

    c1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_053329) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_153830) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_053329) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
